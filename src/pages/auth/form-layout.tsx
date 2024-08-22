674244d9c661cc1ca80e4bf2715c031179669a23
danteBenitez <benitezdante123@gmail.com>
2024-08-22 20:10:42 -0300

    fix: Añadidos campos faltantes a formulario y corregidos estilos

--- src/pages/auth/form-layout.tsx
+++ src/pages/auth/form-layout.tsx
@@ -1,4 +1,3 @@
-import signInBg from "@/assets/img/sign-in-bg.jpg";
 import useAuth from "@/features/auth/hooks/use-auth";
 import FullScreenSpinner from "@/features/ui/fullscreen-spinner";
 import { Link, Navigate, Outlet } from "react-router-dom";
@@ -12,14 +11,17 @@ export default function FormLayout() {
 
   return (
     <main className="h-full py-10 px-4 md:px-8 md:py-4 xl:p-0 flex items-center">
-      <div className="md:bg-[#f7d7c8] h-full border rounded-lg flex mx-auto lg:px-0 w-full lg:max-w-6xl xl:max-w-max overflow-hidden shadow-md">
+      <div className="h-full border rounded-lg flex mx-auto lg:px-0 w-full lg:max-w-6xl xl:max-w-max overflow-hidden shadow-md">
         <section className="h-full w-full flex-shrink-1 hidden md:flex md:items-center xl:p-24">
           {/* TODO: Maybe limit the image's width or height to prevent a blurry effect */}
-          <img src={signInBg} className="object-contain xl:min-w-[734px]" />
+          <img
+            src={"/feature-posts.svg"}
+            className="object-contain xl:min-w-[734px] xl:max-w-[900px]"
+          />
         </section>
-        <section className="flex bg-background flex-col justify-center gap-3 p-4 py-2 text-lg xl:w-2/3 md:px-10 w-full max-h-min">
+        <section className="flex bg-background flex-col justify-center gap-3 p-4 py-2 text-lg xl:w-2/3 md:px-10 w-full max-h-min min-w-[400px]">
           <Link to="/" className="flex items-center max-w-4xl">
-            <h1 className="font-sans-accent text-7xl text-foreground">
+            <h1 className="font-sans-accent text-5xl text-foreground">
               ComunidadActiva
             </h1>
           </Link>
