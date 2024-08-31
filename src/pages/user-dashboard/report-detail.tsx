88d3ae8d3fee2e39c7d07746320ad1f2678284b4
alexsh_home <alexsh882@gmail.com>
2024-08-30 22:27:47 -0300

    feat: Actualizar estilos de la página de detalle de reporte

--- src/pages/user-dashboard/report-detail.tsx
+++ src/pages/user-dashboard/report-detail.tsx
@@ -30,7 +30,7 @@ export default function ReportDetailPage() {
   }
 
   return (
-    <main className="px-2 md:px-7 p py-6 overflow-y-scroll max-h-full">
+    <main className="px-2 md:px-7 p py-6 max-h-full">
       <hgroup className="flex gap-2 items-center">
         <div className="flex items-center h-full">
           <Link to={`/dashboard/reports/${isAdmin ? "admin" : "user"}`}>
@@ -57,7 +57,7 @@ export default function ReportDetailPage() {
           <h2 className="font-sans-accent text-2xl mb-6">Título</h2>
           <p>{report?.title}</p>
         </div>
-        <div className="my-2 border-1">
+        <div className="my-2 border-1 text-wrap">
           <h2 className="font-sans-accent text-2xl mb-6">Descripción</h2>
           <p>{report?.description}</p>
         </div>
